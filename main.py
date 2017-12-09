685505c19e3ec251c873a0dc002fa8f8ce2b17cf
Lars Petter Øren Hauge <lpha@statoil.com>
2017-12-09 14:48:55 +0100

    Restructure classes

--- main.py
+++ main.py
@@ -9,34 +9,49 @@ import sys
 import matplotlib
 matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
-from matplotlib import cm
 from matplotlib import patches
 from matplotlib.lines import Line2D
 
-class polybuilder(object):
-    def __init__(self, control, ax, f, fn):
-        self.x = list(control.get_xdata())
-        self.y = list(control.get_ydata())
-        self.canvas = control.figure.canvas
-        self.control = control
-        self.ax = ax
-        self.f = f
+class plotter(object):
+    def __init__(self, fn):
         self.fn = fn
+        self.x = []
+        self.y = []
+        self.fig = None
+        self.canvas = None
+        self.ax = None
+        self.line = None
 
         self.polys = []
         self.last_removed = None
         self.pick = None
 
+        self.keys = {'escape': self.clear,
+                     'enter': self.mkpoly,
+                     'd': self.rmpoly,
+                     'u': self.undo,
+                     'e': self.export
+                     }
+
+        self.plot_segy()
+
+    def plot_segy(self):
+        with segyio.open(self.fn) as f:
+            traces = f.trace.raw[:]
+
+        self.fig, self.ax = plt.subplots()
+        self.ax.imshow(traces, aspect='auto', cmap=plt.get_cmap('BuPu'))
+
+        self.line = Line2D(self.x, self.y, ls='--', c='#666666',
+                      marker='x', mew=2, mec='#204a87', picker=5)
+        self.ax.add_line(self.line)
+
+        self.canvas = self.line.figure.canvas
         self.canvas.mpl_connect('button_release_event', self.onrelease)
         self.canvas.mpl_connect('key_press_event', self.complete)
         self.canvas.mpl_connect('pick_event', self.onpick)
 
-        self.keys = {   'escape': self.clear,
-                        'enter': self.mkpoly,
-                        'd': self.rmpoly,
-                        'u': self.undo,
-                        'e': self.export
-                    }
+        plt.show()
 
     def onrelease(self, event):
         if self.pick is not None:
@@ -44,18 +59,18 @@ class polybuilder(object):
             return
 
         if self.canvas.manager.toolbar._active is not None: return
-        if event.inaxes != self.control.axes: return
+        if event.inaxes != self.line.axes: return
         if event.button != 1: return
 
         self.x.append(event.xdata)
         self.y.append(event.ydata)
 
-        self.control.set_data(self.x, self.y)
+        self.line.set_data(self.x, self.y)
         self.canvas.draw()
 
     def clear(self, *_):
         self.x, self.y = [], []
-        self.control.set_data(self.x, self.y)
+        self.line.set_data(self.x, self.y)
 
     def mkpoly(self, *_):
         poly = patches.Polygon(list(zip(self.x, self.y)), alpha = 0.5)
@@ -65,7 +80,7 @@ class polybuilder(object):
         self.clear()
 
     def rmpoly(self, event):
-        if event.inaxes != self.control.axes: return
+        if event.inaxes != self.line.axes: return
 
         for poly in self.polys:
             if not poly.contains(event)[0]: continue
@@ -87,7 +102,7 @@ class polybuilder(object):
         self.canvas.draw()
 
     def onpick(self, event):
-        if event.artist is not self.control: return
+        if event.artist is not self.line: return
         self.pick = 1
 
     def export(self, *_):
@@ -97,20 +112,20 @@ class polybuilder(object):
             px, py = np.mgrid[0:traces.shape[0], 0:traces.shape[1]]
             points = np.c_[py.ravel(), px.ravel()]
 
-        for poly in self.polys:
-            mask = poly.get_path().contains_points(points)
-            np.place(output, mask, [1])
+            for poly in self.polys:
+                mask = poly.get_path().contains_points(points)
+                np.place(output, mask, [1])
 
-        meta = segyio.tools.metadata(self.f)
-        with segyio.create('labelmade-' + self.fn, meta) as out:
-            out.text[0] = self.f.text
+            meta = segyio.tools.metadata(f)
+            with segyio.create('labelmade-' + self.fn, meta) as out:
+                out.text[0] = f.text
 
-            for i in range(1, 1 + self.f.ext_header):
-                out.text[i] = self.f.text[i]
+                for i in range(1, 1 + f.ext_header):
+                    out.text[i] = f.text[i]
 
-            out.bin = self.f.bin
-            out.header = self.f.header
-            out.trace = output
+                out.bin = f.bin
+                out.header = f.header
+                out.trace = output
 
 def main(argv):
     parser = argparse.ArgumentParser(prog = argv[0],
@@ -118,19 +133,7 @@ def main(argv):
     parser.add_argument('input', type=str, help='input file')
     args = parser.parse_args(args = argv[1:])
 
-    with segyio.open(args.input) as f:
-        traces = f.trace.raw[:]
-        low, high = np.nanmin(traces), np.nanmax(traces)
-
-        _, ax = plt.subplots()
-        ax.imshow(traces, aspect='auto', cmap = plt.get_cmap('BuPu'))
-
-        line = Line2D([], [], ls='--', c='#666666',
-                      marker='x', mew=2, mec='#204a87', picker = 5)
-        ax.add_line(line)
-        pb = polybuilder(line, ax, f, args.input)
-
-        plt.show()
+    plotter(args.input)
 
 if __name__ == '__main__':
     main(sys.argv)
