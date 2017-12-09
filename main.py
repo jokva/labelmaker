d27800c3a99319b6bc03aae4a62018c47f2a79ee
Lars Petter Øren Hauge <lpha@statoil.com>
2017-12-09 14:13:18 +0100

    Add keybinding to export

--- main.py
+++ main.py
@@ -14,12 +14,14 @@ from matplotlib import patches
 from matplotlib.lines import Line2D
 
 class polybuilder(object):
-    def __init__(self, control, ax):
+    def __init__(self, control, ax, f, fn):
         self.x = list(control.get_xdata())
         self.y = list(control.get_ydata())
         self.canvas = control.figure.canvas
         self.control = control
         self.ax = ax
+        self.f = f
+        self.fn = fn
 
         self.polys = []
         self.last_removed = None
@@ -33,6 +35,7 @@ class polybuilder(object):
                         'enter': self.mkpoly,
                         'd': self.rmpoly,
                         'u': self.undo,
+                        'e': self.export
                     }
 
     def onrelease(self, event):
@@ -87,6 +90,27 @@ class polybuilder(object):
         if event.artist is not self.control: return
         self.pick = 1
 
+    def export(self):
+        traces = self.f.raw[:]
+        output = np.zeros(traces.shape, dtype=np.single)
+        px, py = np.mgrid[0:traces.shape[0], 0:traces.shape[1]]
+        points = np.c_[py.ravel(), px.ravel()]
+
+        for poly in self.polys:
+            mask = poly.get_path().contains_points(points)
+            np.place(output, mask, [1])
+
+        meta = segyio.tools.metadata(self.f)
+        with segyio.create('labelmade-' + self.fn, meta) as out:
+            out.text[0] = self.f.text
+
+            for i in range(1, 1 + self.f.ext_header):
+                out.text[i] = self.f.text[i]
+
+            out.bin = self.f.bin
+            out.header = self.f.header
+            out.trace = output
+
 def main(argv):
     parser = argparse.ArgumentParser(prog = argv[0],
                                      description='Label those slices yo')
@@ -103,28 +127,9 @@ def main(argv):
         line = Line2D([], [], ls='--', c='#666666',
                       marker='x', mew=2, mec='#204a87', picker = 5)
         ax.add_line(line)
-        pb = polybuilder(line, ax)
-
-        output = np.zeros(traces.shape, dtype = np.single)
-        px, py = np.mgrid[0:traces.shape[0], 0:traces.shape[1]]
-        points = np.c_[py.ravel(), px.ravel()]
+        pb = polybuilder(line, ax, f, args.input)
 
         plt.show()
-
-        for poly in pb.polys:
-            mask = poly.get_path().contains_points(points)
-            np.place(output, mask, [1])
-
-        meta = segyio.tools.metadata(f)
-        with segyio.create('labelmade-' + args.input, meta) as out:
-            out.text[0] = f.text
-
-            for i in range(1, 1 + f.ext_header):
-                out.text[i] = f.text[i]
-
-            out.bin = f.bin
-            out.header = f.header
-            out.trace = output
 
 if __name__ == '__main__':
     main(sys.argv)
